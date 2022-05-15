531509755fde8a3e3d0db01162abe7faf620b333
vol_dev <voladores@inbox.ru>
2022-05-15 13:57:36 +0300

    fix template

--- js/template.js
+++ js/template.js
@@ -1,4 +1,6 @@
+import { DEFAULT_VALUES } from './model.js'
+
 export const OUTPUT = `<div class="output">
 <div data-previous-operand class="previous-operand"></div>
-<div data-current-operand class="current-operand">0</div>
+<div data-current-operand class="current-operand">${DEFAULT_VALUES.currentOperand}</div>
 </div>`
